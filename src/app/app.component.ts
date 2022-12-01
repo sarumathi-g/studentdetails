e023bd5a5693a0d21dc75cd08cd38220c91c9332
SARUMATHI  G <ggsarumathi@gmail.com>
2022-12-01 23:34:30 +0530

    created application

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "studentdetails";
+  // fname="saru"
+  // rollno='142';
+  name:any;
+  id:any;
+  department!:string;
+
+
+}
+
+
